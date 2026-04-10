db34015c1d7946d3d9e81161ca243ee498910401
pranavinox <manumanu54482@gmail.com>
2026-04-10 17:54:19 +0530

    added features

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const menu = document.getElementById('menu');
+const menu = document.querySelector('.menu');
+const menu2 = document.querySelector('.sidebar')
+  const links = document.querySelectorAll("things");
 
-function dishes(){
-    window.scroll(menu)
+  links.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href");
+      const targetSection = document.querySelector(targetId);
+
+      targetSection.scrollIntoView({
+        behavior: "smooth"
+      });
+
+      // Close sidebar (if open)
+      document.getElementById("sidebar").style.right = "-250px";
+    });
+  });
+
+function menushow(){
+    menu2.style.display = 'flex'
+}
+
+function menuoff(){
+    menu2.style.display = 'none'
 }
